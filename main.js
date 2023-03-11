7f52e70ed2955ff789d45bdfef3988a02529f1f7
Molloy <2753219155@qq.com>
2023-03-11 17:30:42 +0800

    add

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-const { app, BrowserWindow } = require('electron');
+const {app, BrowserWindow,} = require('electron');
+const {autoUpdater, AppUpdater} = require("electron-updater");
 const path = require('path');
 
+//Basic flags
+autoUpdater.autoDownload = false;
+autoUpdater.autoInstallOnAppQuit = true;
+
+let win = null
+
 const createWindow = () => {
-    const win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 800,
         height: 600,
         webPreferences: {
@@ -11,7 +18,15 @@ const createWindow = () => {
     });
 
     win.loadFile('index.html');
+
+    // Open the DevTools.
+    win.webContents.openDevTools()
 };
+
+const showMessage = (win, value) => {
+    win.webContents.send("update-message", value)
+}
+
 
 app.whenReady().then(() => {
     createWindow();
@@ -21,6 +36,30 @@ app.whenReady().then(() => {
             createWindow();
         }
     });
+
+    autoUpdater.checkForUpdates();
+
+    showMessage(win, 'check-update')
+});
+
+/*New Update Available*/
+autoUpdater.on("update-available", (info) => {
+    showMessage(win, `Update available. Current version ${app.getVersion()}`)
+    let pth = autoUpdater.downloadUpdate();
+    showMessage(pth);
+});
+
+autoUpdater.on("update-not-available", (info) => {
+    showMessage(win, `No update available. Current version ${app.getVersion()}`);
+});
+
+/*Download Completion Message*/
+autoUpdater.on("update-downloaded", (info) => {
+    showMessage(win, `Update downloaded. Current version ${app.getVersion()}`);
+});
+
+autoUpdater.on("error", (info) => {
+    showMessage(win, info);
 });
 
 app.on('window-all-closed', () => {
--- preload.js
+++ preload.js
@@ -1,7 +1,8 @@
-const { contextBridge } = require('electron');
+const {contextBridge, ipcRenderer} = require('electron');
 
 contextBridge.exposeInMainWorld('versions', {
     node: () => process.versions.node,
     chrome: () => process.versions.chrome,
     electron: () => process.versions.electron,
+    updateMessage: (callback) => ipcRenderer.on("update-message", callback),
 });
